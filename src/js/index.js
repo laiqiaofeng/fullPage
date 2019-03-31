44090d0917b1c2728c3bfc9f7852e35b825e2b9d
laiqiaofeng <2425138652@qq.com>
2019-03-31 21:58:58 +0800

    仿写fullPage

--- src/js/index.js
+++ src/js/index.js
@@ -8,19 +8,22 @@ $('.fullPage-wra').fullPage({
                     ["#eeeeee", "red"],
                     ["#aaaaaa", "#456566", "#001122"],
                     ["#555000"]],
-    sectionColorArr: ['red', "blue", "yellow", "block", "glay"],
-
+    sectionColorArr: ['red', "blue", "yellow", "block", "glay"],    
+    // sectionBgArr: ["./src/img/timg1.jpeg", "./src/img/timg2.jpeg", "./src/img/timg3.jpeg", "./src/img/timg4.jpeg", "./src/img/timg5.jpeg"],
     // slideLoop: true,
     scrollEvent: true,
     keydownEvent: true,
-    onLeave: function (index, ele) {
-        console.log(index, ele);
+    onLeave: function (index, ele, direction) {
+        // console.log(index, ele);
     },
-    onShow: function (index, ele) {
-        console.log(index, ele)
+    onShow: function (index, ele, direction) {
+        // console.log(index, ele)
     },
-    onSlideIn: function (index, ele) {
-        console.log(index, ele);
+    onSlideIn: function (index, ele, direction) {
+        // console.log(index, ele);
+    },
+    onSlideOut: function (index, ele, direction) {
+        // console.log(direction);
     }
 
 }).addSlide();
